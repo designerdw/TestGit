c8f7268bc8ec193a58112eaaf35d6e8c27fdd6e2
박대웅 <designerdw@h3xb0x.local>
2016-02-24 15:33:04 +0900

    master commit

--- Test3/main.cpp
+++ Test3/main.cpp
@@ -7,9 +7,15 @@
 //
 
 #include <iostream>
+#include <algorithm>
+#include <list>
+#include <cassert>
+
+using namespace std;
 
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
+    
     return 0;
 }
